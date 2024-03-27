df2ce8ba6772163cb0f9df27de4548a50a6807f2
shreya-mishra <shreyamishra040398@gmail.com>
2024-03-27 15:53:25 +0530

    resolved conflicts

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,12 +1,41 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ballPositionType } from "./GamePlayground";
 
-const Circle = () => {
+type CircleProps = {
+  ballPosition: ballPositionType;
+  ballColor: string;
+};
+const Circle = ({ ballPosition, ballColor }: CircleProps) => {
+  const circleSize = 50;
+  const circleOffset = 30;
+
   return (
-    <View>
-      <Text>Circle</Text>
+    <View
+      style={{
+        position: "absolute",
+        top: ballPosition.top,
+        left: ballPosition.left,
+      }}
+    >
+      <View
+        style={[
+          styles.circle,
+          // position,
+          { backgroundColor: ballColor },
+        ]}
+      ></View>
     </View>
   );
 };
 
 export default Circle;
+
+const styles = StyleSheet.create({
+  circle: {
+    height: 25,
+    width: 25,
+    // position: "absolute",
+    borderRadius: 100,
+  },
+});
