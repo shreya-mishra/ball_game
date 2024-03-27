fd8f3346968c6b9139b83db95b99b1caf57de299
shreya-mishra <shreyamishra040398@gmail.com>
2024-03-27 06:26:00 +0530

    removed unused code

--- src/components/GamePlayground.tsx
+++ src/components/GamePlayground.tsx
@@ -1,7 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
-import Target from "./Target";
-import Circle from "./Circle";
 
 export type ballPositionType = { id: number; top: number; left: number };
 type GamePlayGroundType = {
@@ -18,24 +16,8 @@ const GamePlayground = ({
 }: GamePlayGroundType) => {
   return (
     <View style={[styles.gameContainer, { borderWidth: border ? 4 : 0 }]}>
-      {/* target
-      ball (random position) */}
-      <Target
-        targetPositionFromLeft={0}
-        targetPositionFromTop={0}
-        ballPosition={ballPosition}
-      />
-      <View>
-        {/* <Controllers
-          key={statusOfButton}
-          statusOfButton={statusOfButton}
-          disabledGoalButton={disabledGoalButton}
-          moveCircle={moveBallToParticularPos}
-        /> */}
-      </View>
-      {ballPosition && (
-        <Circle ballPosition={ballPosition} ballColor={ballColor} />
-      )}
+      <Target />
+      <Circle ballPosition={ballPosition} ballColor={ballColor} />
     </View>
   );
 };
