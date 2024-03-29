27b95d74479894754603863844e8555629c77ce5
shreya-mishra <shreyamishra040398@gmail.com>
2024-03-29 19:47:23 +0530

    added positing of the ball via controller

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -1,9 +1,21 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { COLORS } from "../constants/colors";
+import { ballPositionType } from "../constants/types";
 
-const Ball = () => {
-  return <View testID="ball" style={styles.ball} />;
+const Ball = ({ ballPosition }: { ballPosition: ballPositionType }) => {
+  console.log("🚀 ~ Ball ~ ballPosition:", ballPosition);
+  return (
+    <View
+      style={{
+        position: "absolute",
+        top: ballPosition.top,
+        left: ballPosition.left,
+      }}
+    >
+      <View testID="ball" style={styles.ball} />
+    </View>
+  );
 };
 
 export default Ball;
