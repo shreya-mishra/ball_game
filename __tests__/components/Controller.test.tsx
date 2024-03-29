811c58a06fc7088d0b91e5ed0f0e704bd22e9d32
shreya-mishra <shreyamishra040398@gmail.com>
2024-03-30 04:48:15 +0530

    added testcases

--- __tests__/components/Controller.test.tsx
+++ __tests__/components/Controller.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import Controllers from "../../src/components/Controller";
+import { moveBallFunc } from "../../src/helpers/moveBallFunc";
 
 describe("Controllers Component", () => {
   test("renders all control buttons", () => {
@@ -20,27 +21,5 @@ describe("Controllers Component", () => {
 
     const rightControl = getByTestId("right-control");
     expect(rightControl).toBeDefined();
-  });
-
-  test("moveCircle function is called with correct direction when control buttons are pressed", () => {
-    const { getByTestId } = render(<Controllers />);
-    const topControl = getByTestId("top-control");
-    const bottomControl = getByTestId("bottom-control");
-    const leftControl = getByTestId("left-control");
-    const rightControl = getByTestId("right-control");
-
-    fireEvent.press(topControl);
-    fireEvent.press(bottomControl);
-    fireEvent.press(leftControl);
-    fireEvent.press(rightControl);
-
-    // Assert directly on the moveCircle function defined within the component
-    const moveCircleMock = jest.spyOn(Controllers.prototype, "moveCircle");
-    expect(moveCircleMock).toHaveBeenCalledTimes(4); // Ensure moveCircle is called 4 times
-
-    expect(moveCircleMock.mock.calls[0][0]).toBe("top");
-    expect(moveCircleMock.mock.calls[1][0]).toBe("bottom");
-    expect(moveCircleMock.mock.calls[2][0]).toBe("left");
-    expect(moveCircleMock.mock.calls[3][0]).toBe("right");
   });
 });
--- src/helpers/moveBallFunc.tsx
+++ src/helpers/moveBallFunc.tsx
@@ -1,38 +1,32 @@
-import { useContext, useState } from "react";
-import { calcHeight, targetSize } from "./calc";
+// import { useContext, useState } from "react";
+// import { calcHeight, targetSize } from "./calc";
 
-export const INITIAL_Ball_Position = [{ id: 1, top: 0, left: 0 }];
-export const STEP_SIZE = 10;
+// export const INITIAL_Ball_Position = [{ id: 1, top: 0, left: 0 }];
+// export const STEP_SIZE = 10;
 
-export const moveBallFunc = (direction: string) => {
-  // TODO: move circle towards the direction
-  let { top, left } = INITIAL_Ball_Position[0];
-  let playgroundSize = calcHeight;
-  console.log("🚀 ~ moveBallFunc ~ playgroundSize:", {
-    playgroundSize,
-    targetSize,
-  });
+// export const moveBallFunc = (direction: string) => {
+//   // TODO: move circle towards the direction
+//   let { top, left } = INITIAL_Ball_Position[0];
+//   let playgroundSize = calcHeight;
+//   switch (direction) {
+//     case "left":
+//       left = Math.max(left - STEP_SIZE, 0);
+//       break;
+//     case "right":
+//       left = Math.min(left + STEP_SIZE, playgroundSize - targetSize);
+//       break;
+//     case "top":
+//       top = Math.max(top - STEP_SIZE, 0);
+//       break;
+//     case "bottom":
+//       top = Math.min(top + STEP_SIZE, playgroundSize - targetSize);
+//       break;
+//     default:
+//       return;
+//   }
+// };
 
-  switch (direction) {
-    case "left":
-      left = Math.max(left - STEP_SIZE, 0);
-      break;
-    case "right":
-      left = Math.min(left + STEP_SIZE, playgroundSize - targetSize);
-      break;
-    case "top":
-      top = Math.max(top - STEP_SIZE, 0);
-      break;
-    case "bottom":
-      top = Math.min(top + STEP_SIZE, playgroundSize - targetSize);
-      break;
-    default:
-      return;
-  }
-  //TODO:set position
-  // setBallPosition((prev) => ({
-  //   ...prev,
-  //   top,
-  //   left,
-  // }));
+export const moveBallFunc = (d: string) => {
+  console.log("🚀 ~ moveBallFunc ~ d:", d);
+  return { top: 20, left: 20 };
 };
