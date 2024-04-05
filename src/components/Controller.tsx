0d8b402fea24fbafd2f736c6507f25d41283609e
shreya-mishra <shreyamishra040398@gmail.com>
2024-04-05 19:12:09 +0530

    removed unused hook

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React, { useContext, useState } from "react";
+import React from "react";
 import { moveBallFunc, usePositionContext } from "../context/positionContext";
 import { BallPositionContext } from "../constants/types";
 
