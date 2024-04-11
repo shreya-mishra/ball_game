2c603e81da93f3b5c9dc6174c50ac5070ec6ab94
SHREYA MISHRA <61110378+shreya-mishra@users.noreply.github.com>
2024-04-11 05:58:16 +0530

    removed commented code

--- src/helpers/moveBallFunc.tsx
+++ src/helpers/moveBallFunc.tsx
@@ -1,31 +1,3 @@
-// import { useContext, useState } from "react";
-// import { calcHeight, targetSize } from "./calc";
-
-// export const INITIAL_Ball_Position = [{ id: 1, top: 0, left: 0 }];
-// export const STEP_SIZE = 10;
-
-// export const moveBallFunc = (direction: string) => {
-//   // TODO: move circle towards the direction
-//   let { top, left } = INITIAL_Ball_Position[0];
-//   let playgroundSize = calcHeight;
-//   switch (direction) {
-//     case "left":
-//       left = Math.max(left - STEP_SIZE, 0);
-//       break;
-//     case "right":
-//       left = Math.min(left + STEP_SIZE, playgroundSize - targetSize);
-//       break;
-//     case "top":
-//       top = Math.max(top - STEP_SIZE, 0);
-//       break;
-//     case "bottom":
-//       top = Math.min(top + STEP_SIZE, playgroundSize - targetSize);
-//       break;
-//     default:
-//       return;
-//   }
-// };
-
 export const moveBallFunc = (d: string) => {
   console.log("🚀 ~ moveBallFunc ~ d:", d);
   return { top: 20, left: 20 };
